e02b8e91e262207a7a7d993d55a37dc145eeaaed
akshay sk <akshaykabburamath@gmail.com>
2026-05-09 13:45:57 +0530

    link ats

--- src/app/(client)/dashboard/resume/page.tsx
+++ src/app/(client)/dashboard/resume/page.tsx
@@ -2,7 +2,7 @@ export default function ResumePage() {
   return (
     <div className="w-full h-[calc(100vh-80px)]">
       <iframe
-        src="https://ats-system-h9js.onrender.com"
+        src="https://ats-system-1-a6dv.onrender.com"
         className="w-full h-full border-none rounded-xl"
       />
     </div>
--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,7 +4,7 @@ export async function POST(req: NextRequest) {
   try {
     const formData = await req.formData();
 
-    const response = await fetch("http://127.0.0.1:8000/analyze", {
+    const response = await fetch("https://ats-system-1-a6dv.onrender.com/analyze", {
       method: "POST",
       body: formData,
     });
